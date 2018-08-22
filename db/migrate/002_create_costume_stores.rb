5566d956e94eb6768ca2220dc69af396e1201e77
tahseen447 Siddiqui <tahseenfatima1986@gmail.com>
2018-08-22 23:42:26 +0000

    automatically backed up by learn

--- db/migrate/002_create_costume_stores.rb
+++ db/migrate/002_create_costume_stores.rb
@@ -2,4 +2,11 @@
 class CostumeStores < ActiveRecord::Migration
 
   def change
+    create_table :costumes do |c|
+      c.string :name
+      c.float :price
+      c.string :size
+      c.string :image_url
+    end
+  end
   end
