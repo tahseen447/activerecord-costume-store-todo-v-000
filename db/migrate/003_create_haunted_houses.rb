ca582881e00ed4a04313c68c524fda48b3cd926d
tahseen447 Siddiqui <tahseenfatima1986@gmail.com>
2018-08-22 23:45:30 +0000

    automatically backed up by learn

--- db/migrate/003_create_haunted_houses.rb
+++ db/migrate/003_create_haunted_houses.rb
@@ -1,10 +1,10 @@
 # Create your haunted_houses migration here
 class CreateHauntedHouses < ActiveRecord::Migration
   def change
-    create_table :costumes do |c|
+    create_table :haunted_houses do |c|
       c.string :name
-      c.float :price
-      c.string :size
+      c.string :location
+      c.string :theme
       c.string :image_url
     end
   end
